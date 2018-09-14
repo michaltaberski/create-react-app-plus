982428548fd0bceff55210077494767f513805c7
michaltaberski <m.taberski@gmail.com>
2018-09-14 20:00:53 +0200

    utils/buildImmerReducer

---
+++ src/utils/buildImmerReducer.js
@@ -0,0 +1,10 @@
+import produce from 'immer'
+
+const buildImmerReducer = (initialState, actionsReducers) => {
+  return produce((state, action) => {
+    const actionReducer = actionsReducers[action.type];
+    if (actionReducer) return actionReducer(state, action);
+  }, initialState);
+};
+
+export default buildImmerReducer;
