368e510b2a1c9190b5f8a00f2ce9d689725df74a
michaltaberski <m.taberski@gmail.com>
2018-12-13 16:13:01 +0100

    Multicounters

--- src/routes/Rx/Rx.js
+++ src/routes/Rx/Rx.js
@@ -6,9 +6,17 @@ const Rx = props => (
   <div>
     <Helmet title="About" />
     <h1>Rx Page</h1>
-    <h2>Count: {props.count}</h2>
-    <button onClick={actions.inc}>+</button>
-    <button onClick={actions.dec}>-</button>
+    <ul>
+      {props.counters.map(({ id, count }) => (
+        <li key={id}>
+          <h2>
+            {id} count: {count}
+          </h2>
+          <button onClick={() => actions.inc(id)}>+</button>
+          <button onClick={() => actions.dec(id)}>-</button>
+        </li>
+      ))}
+    </ul>
     <p>Did you get here via Redux?</p>
   </div>
 );
--- src/routes/Rx/rxStore.js
+++ src/routes/Rx/rxStore.js
@@ -4,15 +4,37 @@ import { startWith, scan } from 'rxjs/operators';
 
 export const action$ = new Subject();
 
+const id = () =>
+  Math.random()
+    .toString(36)
+    .replace(/[^a-z]+/g, '')
+    .substr(0, 5);
+
 // Initial State
-const initState = { count: 0 };
+const initState = {
+  counters: [
+    { id: id(), count: 0 },
+    { id: id(), count: 2 },
+    { id: id(), count: 6 }
+  ]
+};
 
 // Redux reducer
 const reducer = (state, action) => {
   switch (action.type) {
     case 'INC':
-      return { ...state, count: state.count + 1 };
+      const incCounters = [...state.counters];
+      const incIndex = state.counters.findIndex(
+        counter => counter.id === action.payload.id
+      );
+      incCounters[incIndex].count += 1;
+      return { ...state, incCounters };
     case 'DEC':
+      const counters = [...state.counters];
+      const index = state.counters.findIndex(
+        counter => counter.id === action.payload.id
+      );
+      counters[index].count -= 1;
       return { ...state, count: state.count - 1 };
     default:
       return state;
@@ -30,13 +52,21 @@ export const actionDispatcher = func => (...args) =>
 
 // Example action function
 export const actions = {
-  inc: actionDispatcher(() => ({ type: 'INC' })),
-  dec: actionDispatcher(() => ({ type: 'DEC' }))
+  inc: actionDispatcher(id => ({ type: 'INC', payload: { id } })),
+  dec: actionDispatcher(id => ({ type: 'DEC', payload: { id } }))
+};
+
+const getState = () => {
+  let state;
+  store$.subscribe(_state => (state = _state)).unsubscribe();
+  return state;
 };
 
 export const connectRx = (mapStateToProps = state => state) => {
   return WrappedComponent => {
     return class extends React.Component {
+      state = mapStateToProps(getState());
+
       componentDidMount() {
         this.subscriber = store$.subscribe(state => {
           this.setState(mapStateToProps(state));
