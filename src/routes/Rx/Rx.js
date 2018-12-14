6ff72cd702d6377995357d91b429407d34c9a505
michaltaberski <m.taberski@gmail.com>
2018-12-14 08:02:07 +0100

    RxPage 0.1

--- src/routes/Rx/Rx.js
+++ src/routes/Rx/Rx.js
@@ -9,15 +9,33 @@ const Rx = props => (
     <ul>
       {props.counters.map(({ id, count }) => (
         <li key={id}>
-          <h2>
-            {id} count: {count}
-          </h2>
-          <button onClick={() => actions.inc(id)}>+</button>
-          <button onClick={() => actions.dec(id)}>-</button>
+          <small style={{ marginLeft: '10px', color: '#999' }}>#{id}</small>
+          <h3 style={{ margin: '0 0 15px 0' }}>
+            count: {count}
+            &nbsp; &nbsp;
+            <button onClick={() => actions.inc(id)}>+</button>
+            <button onClick={() => actions.dec(id)}>-</button>
+            &nbsp; &nbsp;
+            <button
+              onClick={() =>
+                window.confirm('Are you sure?') && actions.del(id)
+              }>
+              DEL
+            </button>
+          </h3>
         </li>
       ))}
+      <li>
+        <button onClick={() => actions.add()}>ADD</button>
+      </li>
     </ul>
-    <p>Did you get here via Redux?</p>
+    <h4>
+      <button onClick={() => actions.setText('Click! (Hide text after 1s)')}>
+        Click!
+      </button>
+      &nbsp;
+      <span style={{ color: 'red' }}>{props.text}</span>
+    </h4>
   </div>
 );
 
--- src/routes/Rx/rxStore.js
+++ src/routes/Rx/rxStore.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { Subject } from 'rxjs';
-import { startWith, scan } from 'rxjs/operators';
+import {
+  startWith,
+  scan,
+  shareReplay,
+  delay,
+  filter,
+  tap
+} from 'rxjs/operators';
+import produce from 'immer';
 
 export const action$ = new Subject();
 
@@ -12,6 +20,7 @@ const id = () =>
 
 // Initial State
 const initState = {
+  text: '',
   counters: [
     { id: id(), count: 0 },
     { id: id(), count: 2 },
@@ -19,41 +28,81 @@ const initState = {
   ]
 };
 
+const inc = (state, action) =>
+  produce(state, draft => {
+    draft.counters.find(counter => counter.id === action.payload.id).count += 1;
+  });
+
+const dec = (state, action) => {
+  return produce(state, draft => {
+    draft.counters.find(counter => counter.id === action.payload.id).count -= 1;
+  });
+};
+
+const del = (state, action) =>
+  produce(state, draft => {
+    const index = draft.counters.findIndex(
+      counter => counter.id === action.payload.id
+    );
+    draft.counters.splice(index, 1);
+  });
+
+const add = (state, action) =>
+  produce(state, draft => {
+    draft.counters.push({ id: id(), count: 0 });
+  });
+
+const setText = (state, action) =>
+  produce(state, draft => {
+    draft.text = action.payload;
+  });
+
+const clearText = (state, action) =>
+  produce(state, draft => {
+    draft.text = '';
+  });
+
 // Redux reducer
 const reducer = (state, action) => {
-  switch (action.type) {
-    case 'INC':
-      const incCounters = [...state.counters];
-      const incIndex = state.counters.findIndex(
-        counter => counter.id === action.payload.id
-      );
-      incCounters[incIndex].count += 1;
-      return { ...state, incCounters };
-    case 'DEC':
-      const counters = [...state.counters];
-      const index = state.counters.findIndex(
-        counter => counter.id === action.payload.id
-      );
-      counters[index].count -= 1;
-      return { ...state, count: state.count - 1 };
-    default:
-      return state;
-  }
+  const map = new Map([
+    ['INC', inc],
+    ['DEC', dec],
+    ['DEL', del],
+    ['ADD', add],
+    ['SET_TEXT', setText],
+    ['CLEAR_TEXT', clearText]
+  ]);
+  return map.has(action.type) ? map.get(action.type)(state, action) : state;
 };
+
+const clickEpic$ = action$.pipe(
+  filter(action => action.type === 'SET_TEXT'),
+  delay(1000),
+  tap(() => dispatch({ type: 'CLEAR_TEXT' }))
+);
+clickEpic$.subscribe();
 
 export const store$ = action$.pipe(
   startWith(initState),
-  scan(reducer)
+  scan(reducer),
+  shareReplay(1)
 );
 
+// // BASIC LOGGER
+action$.subscribe(state => console.log('A: ', state));
+store$.subscribe(state => console.log('S: ', state));
+
+export const dispatch = action => action$.next(action);
 // Higher order function to send actions to the stream
-export const actionDispatcher = func => (...args) =>
-  action$.next(func(...args));
+export const actionDispatcher = func => (...args) => dispatch(func(...args));
 
 // Example action function
 export const actions = {
   inc: actionDispatcher(id => ({ type: 'INC', payload: { id } })),
-  dec: actionDispatcher(id => ({ type: 'DEC', payload: { id } }))
+  dec: actionDispatcher(id => ({ type: 'DEC', payload: { id } })),
+  del: actionDispatcher(id => ({ type: 'DEL', payload: { id } })),
+  add: actionDispatcher(id => ({ type: 'ADD' })),
+  setText: actionDispatcher(text => ({ type: 'SET_TEXT', payload: text }))
 };
 
 const getState = () => {
